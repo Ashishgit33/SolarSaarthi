8104e60393cb1c5625b64e3abdcbb11e9ae8d266
Ashishgit33 <114179607+Ashishgit33@users.noreply.github.com>
2023-06-23 19:20:08 +0530

    eploy coommit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 let map, marker;
-let locate = document.querySelector(".locate-btn");
+let confirmApiBtn=document.getElementById('confirm-key');
 let drawPolygonBtn = document.querySelector(".draw-polygon");
 let Map, AdvancedMarkerElement;
 let drawingManager;
 let polygon;
-let solarPanels=[]
-
+let solarPanels = [];
 
 function changeLocation() {
   var longitude = document.getElementById("longitude").value;
@@ -19,14 +18,18 @@ function changeLocation() {
     return;
   }
   console.log(parsedLatitude, parsedLongitude);
-  const position = { lat:parsedLatitude, lng:parsedLongitude};
+  const position = { lat: parsedLatitude, lng: parsedLongitude };
   map.setCenter(position);
 }
 
-
-
-
-
+function createLatLngControl(){
+  let locationForm = document.createElement("div");
+  locationForm.setAttribute("class", "location-form");
+  locationForm.innerHTML='<div class="location-form"><div class="input-group"><label for="longitude">Longitude:</label><input type="text" id="longitude" placeholder="Enter longitude" /></div><div class="input-group">        <label for="latitude">Latitude:</label        ><input type="text" id="latitude" placeholder="Enter latitude" />      </div>      <div class="button-group">        <button class="locate-btn" >Locate</button>      </div>    </div>'
+  let locateBtn=locationForm.querySelector('.locate-btn');
+  locateBtn.addEventListener('click',changeLocation);
+  return locationForm;
+}
 
 function initMap() {
   navigator.geolocation.getCurrentPosition(async (position) => {
@@ -35,14 +38,11 @@ function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
       center: pos,
       zoom: 20,
-      mapTypeControl:false,
-      streetViewControl:false,
-      mapTypeId:'satellite',
-
+      mapTypeControl: false,
+      streetViewControl: false,
+      mapTypeId: "satellite",
     });
-    // let div=document.createElement('div')
-    // div.appendChild(document.querySelector('.location-form'))
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(document.querySelector('.location-form'));
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(createLatLngControl());
     drawingManager = new google.maps.drawing.DrawingManager({
       drawingMode: google.maps.drawing.OverlayType.POLYGON,
       drawingControl: true,
@@ -52,8 +52,8 @@ function initMap() {
       },
       polygonOptions: {
         editable: false,
-        draggable:true,
-        clickable:false,
+        draggable: true,
+        clickable: false,
         strokeColor: "#00ffff",
         strokeOpacity: 1,
         strokeWeight: 3,
@@ -75,39 +75,64 @@ function initMap() {
         }
       }
     );
-    google.maps.event.addListener(drawingManager, 'polygoncomplete', function(polygon) {
-      polygon.getPath().forEach((latlng)=>{
-        console.log(latlng.toString())
-      })
-    });
-    map.addListener('click',(event)=>{
-      let topleft=event.latLng;
-      let topright=google.maps.geometry.spherical.computeOffset(topleft, 1.7, 180);
-      let bottomright=google.maps.geometry.spherical.computeOffset(topright,1,90);
-      let bottomleft=google.maps.geometry.spherical.computeOffset(bottomright,1.7,0);
-      console.log(topleft.toString())
+    google.maps.event.addListener(
+      drawingManager,
+      "polygoncomplete",
+      function (polygon) {
+        polygon.getPath().forEach((latlng) => {
+          console.log(latlng.toString());
+        });
+      }
+    );
+    map.addListener("click", (event) => {
+      let topleft = event.latLng;
+      let topright = google.maps.geometry.spherical.computeOffset(
+        topleft,
+        1.7,
+        180
+      );
+      let bottomright = google.maps.geometry.spherical.computeOffset(
+        topright,
+        1,
+        90
+      );
+      let bottomleft = google.maps.geometry.spherical.computeOffset(
+        bottomright,
+        1.7,
+        0
+      );
+      console.log(topleft.toString());
       const solarPanel = new google.maps.Polygon({
-        path:[topleft,topright,bottomright,bottomleft],
+        path: [topleft, topright, bottomright, bottomleft],
         strokeColor: "#000080",
         strokeOpacity: 0.8,
         strokeWeight: 2,
         fillColor: "#000080",
         fillOpacity: 0.75,
-        draggable:true,
+        draggable: true,
       });
-      solarPanels.push(solarPanel)
-      solarPanel.addListener('dblclick',(event)=>{
+      solarPanels.push(solarPanel);
+      solarPanel.addListener("dblclick", (event) => {
         solarPanel.setMap(null);
-        solarPanels=solarPanels.filter(item=>item!==solarPanel)
-      })
-      solarPanel.addListener('dragend',(event)=>{
-        
-      })
+        solarPanels = solarPanels.filter((item) => item !== solarPanel);
+      });
+      solarPanel.addListener("dragend", (event) => {});
       solarPanel.setMap(map);
-    })
+    });
   });
 }
+function loadMapScript(){
+  let key=document.getElementById('api-key').value;
+  if(!key) return;
+  console.log(key)
+  let mapScriptSrc=`https://maps.googleapis.com/maps/api/js?key=${key}&libraries=drawing,geometry&callback=initMap`;
+  console.log(mapScriptSrc)
+  let script=document.createElement('script',mapScriptSrc);
+  script.type= 'text/javascript';
+  script.setAttribute('src',mapScriptSrc);
+  document.head.appendChild(script);
+  document.body.removeChild(document.body.querySelector('.api-form'));
+}
 
-
-locate.addEventListener("click", changeLocation);
+confirmApiBtn.addEventListener('click',loadMapScript);
 window.initMap = initMap;
